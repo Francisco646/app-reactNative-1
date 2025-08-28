45ee92c9c5c8d2cb488b720085c031be5c028d09
Francisco646 <practicas_3@Francisco.>
2025-08-28 12:37:10 +0200

    Added backend logic to retrieve the material from an activity

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'mysql-3b4a204d-franciscosanchoval-9361.e.aivencloud.com',
     user: 'avnadmin',
-    password: 'AVNS_5SDeuAlKA7-7YZ_CwQB',
+    password: PROCESS.ENV.db_password,
     database: 'defaultdb',
     port: 11667,
     waitForConnections: true,
